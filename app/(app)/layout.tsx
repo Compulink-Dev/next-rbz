081c26bffcf887e28b7ef4f1faaee39f1142008d
Compulink-Dev <devcompulink@gmail.com>
2025-09-08 16:34:48 +0200

    chatbot

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Chatbot from "@/components/Chatbot";
+// import Chatbot from "@/components/Chatbot";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <main className="">{children}</main>
-        <Chatbot />
+        {/* <Chatbot /> */}
       </body>
     </html>
   );
